bc313ab8297bbab807c8c5e3c6d7bc1f8a56dc55
Matt Nawrocki <mjnrock@umich.edu>
2020-02-21 16:47:19 -0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Lux from "@lespantsfancy/lux";
 
-import Paco from "./package";
+import Paco from "./core/package";
 
 const Raccoon = new Paco.Entity.Raccoon();
 
